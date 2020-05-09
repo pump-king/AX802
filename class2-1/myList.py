d33c5119fb5839bdf89057ff07bfe4076e38cb02
pump-king <merlin.yao@gmail.com>
2020-05-09 11:57:01 +0800

    class2-1

--- class2-1/myList.py
+++ class2-1/myList.py
@@ -5,11 +5,33 @@ Created on Sat May  9 11:18:09 2020
 @author: marco
 """
 
-import linkingList
+
+class node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class linkingList:
+    def __init__(self):
+        self.head = node(0)
+    def trace(self):
+        run = self.head.next
+        while run != None:
+            print(run.value)
+            run = run.next
+    def append(self,value):
+        run = self.head
+        while run.next != None:
+            run = run.next
+        run.next = node(value)
         
-        
-my = linkingList()
-my.trace()
-for i in range(10):
-    my.append(i)
-my.trace()
+    def delete(self,value):
+        pre = self.head
+        run = self.head.next
+        while run:
+            if run.value == value:
+                pre.next = run.next
+                run = run.next
+            else:
+                pre = pre.next
+                run = run.next
--- class2-1/串列.py
+++ class2-1/串列.py
@@ -5,49 +5,10 @@ Created on Sat May  9 09:04:55 2020
 @author: marco
 """
 
-class node:
-    def __init__(self,value):
-        self.value = value
-        self.next = None
-"""
-myNode = node(1)
-myNode.next = node(2)
-myNode.next.next = node(3)
-myNode.next.next.next = node(4)
-myNode.next.next.next.next = node(5)
-"""
-class linkingList:
-    def __init__(self):
-        self.head = node(0)
-    def trace(self):
-        run = self.head.next
-        while run != None:
-            print(run.value)
-            run = run.next
-    def append(self,value):
-        run = self.head
-        while run.next != None:
-            run = run.next
-        run.next = node(value)
-        
-        
-my = linkingList()
+import myList
+my = myList.linkingList()
 my.trace()
 for i in range(10):
     my.append(i)
+my.delete(5)
 my.trace()
-    
-"""
-run = myNode
-while run.next != None:
-    print(run.value)
-    run = run.next
-
-print("-----")
-run.next = node(6)
-
-run = myNode
-while run != None:
-    print(run.value)
-    run = run.next
-"""
