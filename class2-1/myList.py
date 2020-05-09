db2b686eea94d3760dbbe09ffbe2178786f7c373
pump-king <merlin.yao@gmail.com>
2020-05-09 11:30:13 +0800

    init

---
+++ class2-1/myList.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  9 11:18:09 2020
+
+@author: marco
+"""
+
+import linkingList
+        
+        
+my = linkingList()
+my.trace()
+for i in range(10):
+    my.append(i)
+my.trace()
